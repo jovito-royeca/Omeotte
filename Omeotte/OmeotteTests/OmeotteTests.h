21f3dade7877e93c4669ad45e1ef8f31b58a7b2f
Jovito Royeca <buttmanizer@gmail.com>
2013-08-19 22:15:37 +0800

    Created the XCode project.

---
+++ Omeotte/OmeotteTests/OmeotteTests.h
@@ -0,0 +1,13 @@
+//
+//  OmeotteTests.h
+//  OmeotteTests
+//
+//  Created by Jovito Royeca on 8/19/13.
+//  Copyright (c) 2013 JJJ Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OmeotteTests : SenTestCase
+
+@end
---
+++ Omeotte/OmeotteTests/OmeotteTests.m
@@ -0,0 +1,32 @@
+//
+//  OmeotteTests.m
+//  OmeotteTests
+//
+//  Created by Jovito Royeca on 8/19/13.
+//  Copyright (c) 2013 JJJ Software. All rights reserved.
+//
+
+#import "OmeotteTests.h"
+
+@implementation OmeotteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OmeotteTests");
+}
+
+@end
