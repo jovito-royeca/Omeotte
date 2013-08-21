123615993246c7fd1e3eb301944a5d4c4f4e6754
Jovito Royeca <buttmanizer@gmail.com>
2013-08-21 22:25:46 +0800

    Improvements in the Card class.

--- Omeotte/Omeotte/Omeotte.c
+++ Omeotte/Omeotte/Omeotte.c
@@ -7,3 +7,197 @@
 //
 
 #include <stdio.h>
+
+#include "Omeotte.h"
+
+int get_statsfield(StatFields field, Stats stats)
+{
+    switch (field)
+    {
+        case CurrentTower:
+        {
+            return stats->tower;
+        }
+        case CurrentWall:
+        {
+            return stats->wall;
+        }
+        case CurrentBricks:
+        {
+            return stats->bricks;
+        }
+        case CurrentGems:
+        {
+            return stats->gems;
+        }
+        case CurrentRecruits:
+        {
+            return stats->recruits;
+        }
+        case CurrentQuarries:
+        {
+            return stats->quarries;
+        }
+        case CurrentMagics:
+        {
+            return stats->magics;
+        }
+        case CurrentDungeons:
+        {
+            return stats->dungeons;
+        }
+        case None:
+        default:
+        {
+            return 0;
+        }
+    }
+}
+
+void set_statsfield(StatFields field, int value, Stats current, Stats opponent)
+{
+    switch (field)
+    {
+        case CurrentTower:
+        {
+            current->tower += value;
+            break;
+        }
+        case CurrentWall:
+        {
+            current->wall += value;
+            break;
+        }
+        case CurrentBricks:
+        {
+            current->bricks += value;
+            break;
+        }
+        case CurrentGems:
+        {
+            current->gems += value;
+            break;
+        }
+        case CurrentRecruits:
+        {
+            current->recruits += value;
+            break;
+        }
+        case CurrentQuarries:
+        {
+            current->quarries += value;
+            break;
+        }
+        case CurrentMagics:
+        {
+            current->magics += value;
+            break;
+        }
+        case CurrentDungeons:
+        {
+            current->dungeons += value;
+            break;
+        }
+        case OpponentTower:
+        {
+            opponent->tower += value;
+            break;
+        }
+        case OpponentWall:
+        {
+            opponent->wall += value;
+            break;
+        }
+        case OpponentBricks:
+        {
+            opponent->bricks += value;
+            break;
+        }
+        case OpponentGems:
+        {
+            opponent->gems += value;
+            break;
+        }
+        case OpponentRecruits:
+        {
+            opponent->recruits += value;
+            break;
+        }
+        case OpponentQuarries:
+        {
+            opponent->quarries += value;
+            break;
+        }
+        case OpponentMagics:
+        {
+            opponent->magics += value;
+            break;
+        }
+        case OpponentDungeons:
+        {
+            opponent->dungeons += value;
+            break;
+        }
+        case None:
+        {
+            break;
+        }
+    }
+}
+
+void set_stats(Stats src, Stats dest)
+{
+    dest->tower    += src->tower;
+    dest->wall     += src->wall;
+    dest->bricks   += src->bricks;
+    dest->gems     += src->gems;
+    dest->recruits += src->recruits;
+    dest->quarries += src->quarries;
+    dest->magics   += src->magics;
+    dest->dungeons += src->dungeons;
+}
+
+int compare(int op1, int op2)
+{
+    int result = 0;
+    
+    if (op1 < op2)
+    {
+        result = -1;
+    }
+    else if (op1 == op2)
+    {
+        result = 0;
+    }
+    else if (op1 > op2)
+    {
+        result = 1;
+    }
+    
+    return result;
+}
+
+void do_ops(Ops ops, Stats current, Stats opponent)
+{
+    int op1Value = get_statsfield(ops->op1, current);
+    int opValue2 = get_statsfield(ops->op2, current);
+    int opResult = compare(op1Value, opValue2);
+    
+    switch (ops->opType)
+    {
+        case Eval:
+        {
+            if (opResult == ops->opResult)
+            {
+                set_statsfield(ops->opTarget, ops->opResult, current, opponent);
+            }
+            break;
+        }
+        case Eq:
+        {
+            break;
+        }
+        default:
+            break;
+    }
+}
--- Omeotte/Omeotte/Omeotte.h
+++ Omeotte/Omeotte/Omeotte.h
@@ -9,6 +9,77 @@
 #ifndef Omeotte_Omeotte_h
 #define Omeotte_Omeotte_h
 
+#define MAX_PLAYER_HAND 6
 
+#define DECK_SIZE       500
+
+enum
+{
+    Quarry = 0,
+    Magic,
+    Dungeon
+};
+typedef int CardType;
+
+typedef struct
+{
+    int tower;
+    int wall;
+    
+    int bricks;
+    int gems;
+    int recruits;
+    
+    int quarries;
+    int magics;
+    int dungeons;
+} *Stats;
+
+typedef enum
+{
+    CurrentTower     = 0,
+    CurrentWall      = 1,
+    CurrentBricks    = 2,
+    CurrentGems      = 3,
+    CurrentRecruits  = 4,
+    CurrentQuarries  = 5,
+    CurrentMagics    = 6,
+    CurrentDungeons  = 7,
+    OpponentTower    = 8,
+    OpponentWall     = 9,
+    OpponentBricks   = 10,
+    OpponentGems     = 11,
+    OpponentRecruits = 12,
+    OpponentQuarries = 13,
+    OpponentMagics   = 14,
+    OpponentDungeons = 15,
+    None             = -1
+} StatFields;
+
+enum
+{
+    Eval = 0,
+    Eq,
+};
+typedef int OpType;
+
+typedef struct
+{
+    int op1;
+    int op2;
+    int opResult;
+    int opTarget;
+    int opValue;
+    OpType opType;
+} *Ops;
+
+#define create_stats ((Stats)malloc(sizeof(Stats)))
+#define create_ops  ((Ops)malloc(sizeof(Ops)))
+
+int get_statsfield(StatFields field, Stats stats);
+void set_statsfield(StatFields field, int value, Stats current, Stats opponent);
+void set_stats(Stats src, Stats dest);
+int compare(int op1, int op2);
+void do_ops(Ops ops, Stats current, Stats opponent);
 
 #endif
