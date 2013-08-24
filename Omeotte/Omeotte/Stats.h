b40a2161929a369f8b39524f699356b238b575a0
Jovito Royeca <buttmanizer@gmail.com>
2013-08-24 21:08:01 +0800

    Added the Stats class

---
+++ Omeotte/Omeotte/Stats.h
@@ -0,0 +1,13 @@
+//
+//  Stats.h
+//  Omeotte
+//
+//  Created by Jovito Royeca on 8/23/13.
+//  Copyright (c) 2013 JJJ Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stats : NSObject
+
+@end
---
+++ Omeotte/Omeotte/Stats.m
@@ -0,0 +1,13 @@
+//
+//  Stats.m
+//  Omeotte
+//
+//  Created by Jovito Royeca on 8/23/13.
+//  Copyright (c) 2013 JJJ Software. All rights reserved.
+//
+
+#import "Stats.h"
+
+@implementation Stats
+
+@end
