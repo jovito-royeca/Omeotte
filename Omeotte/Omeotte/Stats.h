332270b055104e974b86b482e341f82c169f1dcb
Jovito Royeca <buttmanizer@gmail.com>
2013-08-24 21:09:28 +0800

    Added the Stats class.

--- Omeotte/Omeotte/Stats.h
+++ Omeotte/Omeotte/Stats.h
@@ -8,6 +8,65 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum
+{
+    None     = -1,
+    Tower    = 0,
+    Wall     = 1,
+    Bricks   = 2,
+    Gems     = 3,
+    Recruits = 4,
+    Quarries = 5,
+    Magics   = 6,
+    Dungeons = 7
+} StatField;
+
+enum
+{
+    Current = 0,
+    Opponent,
+};
+typedef int EffectTarget;
+
+typedef struct _Effect
+{
+    StatField field;
+    int value;
+    EffectTarget target;
+} *Effect;
+
+typedef struct _Eval
+{
+    Effect op1;
+    Effect op2;
+    Effect lesserThanResult;
+    Effect equalsResult;
+    Effect greaterThanResult;
+} *Eval;
+
 @interface Stats : NSObject
+
+@property(nonatomic) int tower;
+@property(nonatomic) int wall;
+
+@property(nonatomic) int bricks;
+@property(nonatomic) int gems;
+@property(nonatomic) int recruits;
+
+@property(nonatomic) int quarries;
+@property(nonatomic) int magics;
+@property(nonatomic) int dungeons;
+
++(void) applyEffect:(Effect)effect onCurrent:(Stats*)current  orOpponent:(Stats*) opponent;
+
++(void) evaluate:(Eval)eval onCurrent:(Stats*) current  orOpponent:(Stats*) opponent;
+
+-(int) statField:(StatField)field;
+
+-(void) setStatField:(StatField)field withValue:(int)value;
+
+-(void) setStats:(Stats*)src;
+
+-(int) compare:(int)op1 with:(int)op2;
 
 @end
--- Omeotte/Omeotte/Stats.m
+++ Omeotte/Omeotte/Stats.m
@@ -10,4 +10,203 @@
 
 @implementation Stats
 
+@synthesize tower;
+@synthesize wall;
+
+@synthesize bricks;
+@synthesize gems;
+@synthesize recruits;
+
+@synthesize quarries;
+@synthesize magics;
+@synthesize dungeons;
+
++(void) applyEffect:(Effect)effect onCurrent:(Stats*)current  orOpponent:(Stats*) opponent
+{
+    switch (effect->target)
+    {
+        case Current:
+        {
+            [current setStatField:effect->field withValue:effect->value];
+            break;
+        }
+        case Opponent:
+        {
+            [opponent setStatField:effect->field withValue:effect->value];
+            break;
+        }
+    }
+}
+
++(void) evaluate:(Eval)eval onCurrent:(Stats*) current  orOpponent:(Stats*) opponent
+{
+    
+}
+
+-(int) statField:(StatField)field
+{
+    switch (field)
+    {
+        case Tower:
+        {
+            return self.tower;
+        }
+        case Wall:
+        {
+            return self.wall;
+        }
+        case Bricks:
+        {
+            return self.bricks;
+        }
+        case Gems:
+        {
+            return self.gems;
+        }
+        case Recruits:
+        {
+            return self.recruits;
+        }
+        case Quarries:
+        {
+            return self.quarries;
+        }
+        case Magics:
+        {
+            return self.magics;
+        }
+        case Dungeons:
+        {
+            return self.dungeons;
+        }
+        case None:
+        default:
+        {
+            return 0;
+        }
+    }
+}
+
+-(void) setStatField:(StatField)field withValue:(int)value
+{
+    switch (field)
+    {
+        case Tower:
+        {
+            self.tower += value;
+            break;
+        }
+        case Wall:
+        {
+            self.wall += value;
+            
+            if (self.wall < 0)
+            {
+                self.tower += self.wall;
+                self.wall = 0;
+            }
+            break;
+        }
+        case Bricks:
+        {
+            self.bricks += value;
+            
+            if (self.bricks < 0)
+            {
+                self.bricks = 0;
+            }
+            break;
+        }
+        case Gems:
+        {
+            self.gems += value;
+            
+            if (self.gems < 0)
+            {
+                self.gems = 0;
+            }
+            break;
+        }
+        case Recruits:
+        {
+            self.recruits += value;
+            
+            if (self.recruits < 0)
+            {
+                self.recruits = 0;
+            }
+            break;
+        }
+        case Quarries:
+        {
+            self.quarries += value;
+            
+            if (self.quarries < 0)
+            {
+                self.quarries = 0;
+            }
+            break;
+        }
+        case Magics:
+        {
+            self.magics += value;
+            
+            if (self.magics < 0)
+            {
+                self.magics = 0;
+            }
+            break;
+        }
+        case Dungeons:
+        {
+            self.dungeons += value;
+            
+            if (self.dungeons < 0)
+            {
+                self.dungeons = 0;
+            }
+            break;
+        }
+        case None:
+        default:
+        {
+            break;
+        }
+    }
+}
+
+
+
+-(void) setStats:(Stats*)src
+{
+    [self setStatField:Tower withValue:src.tower];
+    [self setStatField:Wall withValue:src.wall];
+    [self setStatField:Bricks withValue:src.bricks];
+    [self setStatField:Gems withValue:src.gems];
+    [self setStatField:Recruits withValue:src.recruits];
+    [self setStatField:Quarries withValue:src.quarries];
+    [self setStatField:Magics withValue:src.magics];
+    [self setStatField:Dungeons withValue:src.dungeons];
+}
+
+-(int) compare:(int)op1 with:(int)op2
+{
+    int result = 0;
+    
+    if (op1 < op2)
+    {
+        result = -1;
+    }
+    else if (op1 == op2)
+    {
+        result = 0;
+    }
+    else if (op1 > op2)
+    {
+        result = 1;
+    }
+    
+    return result;
+}
+
 @end
